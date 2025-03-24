cf1b6137bf56dd720ddafb79a0ecc713143572ec
Amarildo Golloshi <golloshiamarildo@gmail.com>
2025-03-23 20:44:27 -0400

    Updates

---
+++ data.js
@@ -0,0 +1,54 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const PRODUCTS = [   
+    {
+        id: uuidv4(),
+        name: 'Laptop',
+        price: 999.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Headphones',
+        price: 99.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Mouse',
+        price: 29.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Keyboard',
+        price: 49.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Monitor',
+        price: 399.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Microphone',
+        price: 79.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Webcam',
+        price: 49.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Desk',
+        price: 199.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Chair',
+        price: 149.99,
+    },
+    {
+        id: uuidv4(),
+        name: 'Backpack',
+        price: 39.99,
+    }
+]
